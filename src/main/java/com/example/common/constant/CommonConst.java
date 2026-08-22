b84d617ff2b5580c77d7c1e526345d79d2401cfd
xiansen <116627127+xiansen11@users.noreply.github.com>
2026-08-23 06:13:18 +0800

    feat: publish maoyan common library

---
+++ src/main/java/com/example/common/constant/CommonConst.java
@@ -0,0 +1,18 @@
+package com.example.common.constant;
+
+/**
+ * 通用 Header/常量
+ */
+public final class CommonConst {
+
+    public static final String HEADER_AUTH = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+
+    public static final String CLAIM_USER_ID = "userId";
+    public static final String CLAIM_USERNAME = "username";
+    public static final String CLAIM_ROLE = "role";
+    public static final String CLAIM_CINEMA_ID = "cinemaId";
+
+    private CommonConst() {
+    }
+}
